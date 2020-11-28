61e2338e566c4c04fd07ed8ce811b5649c92ce01
Pranoy <pranoy.mbstu12001@gmail.com>
2020-11-28 16:56:41 +0600

    configure react app and transfer my code to reactive

---
+++ frontend/src/App.js
@@ -0,0 +1,196 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div class="grid-container">
+        <header class="row">
+            <div>
+                <a href="index.html" class="brand">Amazona</a>
+            </div>
+            <div>
+                <a href="cart.html">Cart</a>
+                <a href="signin.html">Sign In</a>
+            </div>
+        </header>
+
+        <main>
+            <div class="row center">
+                <div class="card">
+                    <a href="product.html">
+                        <img class="medium" src="./images/shirt1.png" alt="product" />
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2>Nike Slim Shirt</h2>
+                        </a>
+                    </div>
+                    <div class="rating">
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    </div>
+
+                    <div class="price">
+                        $120
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="product.html">
+                        <img class="medium" src="./images/shirt1.png" alt="product" />
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2>Nike Slim Shirt</h2>
+                        </a>
+                    </div>
+                    <div class="rating">
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    </div>
+
+                    <div class="price">
+                        $120
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="product.html">
+                        <img class="medium" src="./images/shirt1.png" alt="product" />
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2>Nike Slim Shirt</h2>
+                        </a>
+                    </div>
+                    <div class="rating">
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    </div>
+
+                    <div class="price">
+                        $120
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="product.html">
+                        <img class="medium" src="./images/shirt1.png" alt="product" />
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2>Nike Slim Shirt</h2>
+                        </a>
+                    </div>
+                    <div class="rating">
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    </div>
+
+                    <div class="price">
+                        $120
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="product.html">
+                        <img class="medium" src="./images/shirt1.png" alt="product" />
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2>Nike Slim Shirt</h2>
+                        </a>
+                    </div>
+                    <div class="rating">
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    </div>
+
+                    <div class="price">
+                        $120
+                    </div>
+                </div>
+                <div class="card">
+                    <a href="product.html">
+                        <img class="medium" src="./images/shirt1.png" alt="product" />
+                    </a>
+                    <div class="card-body">
+                        <a href="product.html">
+                            <h2>Nike Slim Shirt</h2>
+                        </a>
+                    </div>
+                    <div class="rating">
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                        <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    </div>
+
+                    <div class="price">
+                        $120
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <footer class="row center">
+            All rights reserved
+        </footer>
+    </div>
+    );
+}
+
+export default App;
